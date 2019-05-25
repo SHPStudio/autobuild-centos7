86d28dea6bd5327755c5c660c98890709e8f29c0
Shape <1328445041@qq.com>
2019-05-25 22:44:27 +0800

    修改安装脚本

--- installjdk.sh
+++ installjdk.sh
@@ -25,11 +25,11 @@ fi
 cd devsoft
 if [ ! -f "/usr/local/devsoft/jdk-8u201-linux-x64.tar.gz" ];then
    echo "无jdk包...开始下载.."
-   wget --no-cookie --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jdk-8u201-linux-x64.tar.gz -O jdk-8u201-linux-x64.tar.gz
+   wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz"
 fi
 if [ ! -d "/usr/local/devsoft/jdk1.8" ];then
- tar -zxvf jdk-8u201-linux-x64.tar.gz
- mv jdk1.8.0_201 jdk1.8
+ tar -zxvf jdk-8u141-linux-x64.tar.gz
+ mv jdk1.8.0_141 jdk1.8
 fi
 
 echo "设置环境变量"
